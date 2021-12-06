bf94e1516efc4901abd2244d378bbe141a80f014
Yannick Kinsbergen <ykinsbergen@gofilex.nl>
2021-12-06 11:23:57 +0100

    day 6

---
+++ day-6/index.js
@@ -0,0 +1,25 @@
+const fs = require( "fs" );
+const data = fs.readFileSync( "./data.txt", "utf8" ).split( "," ).map( e => parseInt( e, 10 ) );
+
+const calculatePopulation = ( days ) => {
+  let populationByTimerState = new Array( 9 ).fill( 0 );
+  for ( const timer of data ) {
+    populationByTimerState[ timer ]++;
+  }
+
+  for ( let day = 1; day <= days; day++ ) {
+    const birthingFish = populationByTimerState[ 0 ];
+
+    // move all values one spot up
+    populationByTimerState.copyWithin( 0, 1 );
+
+    populationByTimerState[ 6 ] += birthingFish;
+    populationByTimerState[ 8 ] = birthingFish;
+  }
+  return populationByTimerState.reduce( ( a, c ) => a + c );
+}
+
+// Answer part 1
+console.log( calculatePopulation( 80 ) );
+// Answer part 2
+console.log( calculatePopulation( 256 ) );
